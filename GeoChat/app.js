a32c8e4d13ccc06d1de498f1bbae6036d72868f1
预胜 李 <15178680807@163.com>
2024-12-29 20:14:29 +0800

    局域网实现

--- GeoChat/app.js
+++ GeoChat/app.js
@@ -6,6 +6,7 @@ const messageRouter = require('./routes/message');
 const assessRouter = require('./routes/assess');
 const path = require('path');
 require('dotenv').config();
+const os = require('os');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -25,7 +26,24 @@ app.use('/api/groups', groupRouter);
 app.use('/api/messages', messageRouter);
 app.use('/api/assess', assessRouter);
 
+
+function findWiFiIPv4() {
+  const interfaces = os.networkInterfaces();
+  for (const interfaceName in interfaces) {
+      // 假设 Wi-Fi 接口的名称包含 "Wi-Fi" 或 "wlan" 关键字，不同操作系统可能不同
+      if (interfaceName.toLowerCase().includes('wi-fi') || interfaceName.toLowerCase().includes('wlan')) {
+          const interfaceData = interfaces[interfaceName];
+          for (const data of interfaceData) {
+              // 查找 IPv4 地址，排除环回地址和内部网络地址
+              if (data.family === 'IPv4' &&!data.internal) {
+                  return data.address;
+              }
+          }
+      }
+  }
+  return '127.0.0.1';
+}
 // 启动服务器
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(port,findWiFiIPv4(), () => {
+  console.log(`Server is running on port ${findWiFiIPv4()}:${port}`);
 });
